f3b1a154ffead308eaf8f4a3666104b044d87419
Saipavan2807 <69067018+Saipavan2807@users.noreply.github.com>
2021-09-08 15:20:54 +0530

    Update fun.h

--- 3_Implementation/inc/fun.h
+++ 3_Implementation/inc/fun.h
@@ -1,1 +1,8 @@
-
+int isLeapYear( int year );        
+int leapYears( int year );        
+int todayOf( int y, int m, int d); 
+long days( int y, int m, int d);   
+void calendar(int y, int m);       
+int getDayNumber(int d,int m,int y);
+char *getName(int day);
+void flush();
